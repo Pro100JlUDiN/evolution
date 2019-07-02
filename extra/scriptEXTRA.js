188b59a21d82e8336550dc0b485afbd715b0c80a
Fazetdinov Dmitriy <pro100jludin@gmail.com>
2019-07-03 02:37:21 +0500

    Дополнительное задание в процессе

--- extra/scriptEXTRA.js
+++ extra/scriptEXTRA.js
@@ -1,1 +1,11 @@
-var num;
+let num = 266219;
+let strArray;
+let numArray = [];
+strArray = num.toString().split('');
+
+let multiplication;
+for(i = 0; i < strArray.length; i++){
+    let a = Number(strArray[i]);
+    numArray *= a;
+    console.log(numArray);
+}

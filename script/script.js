8a59aa526e39ae3fb96322ba555bba5a757ee629
Fazetdinov Dmitriy <pro100jludin@gmail.com>
2019-07-03 22:19:50 +0500

    Черновик с функциями

--- script/script.js
+++ script/script.js
@@ -1,76 +1,108 @@
 'use stric';
-// всплывающая тема
+// // приветствие
 // alert('Поздоровался, и шо?');
 
-let money = 1000000; 
-let income = 500000;
-let addExpenses = "Корм для меня, Ипотека, Корм для Кошки"; 
-let deposit = false; 
-let mission = 15000000; 
-let period = 6;
+// let money = 1000000; 
+// let income = 'проточка черенков для лопат';
+// let addExpenses = "Корм для меня, Ипотека, Корм для Кошки"; 
+// let deposit = false; 
+// let mission = 15000000; 
+// let period = 6;
 
 
-console.log('Больше ничего не уметь.');
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(deposit));
+// console.log('Больше ничего не уметь.');
+// console.log(typeof(money));
+// console.log(typeof(income));
+// console.log(typeof(deposit));
 
-console.log(income.toString().length);
+// console.log(income.toString().length);
 
-console.log("Период " + period +  " месяцев");
-console.log("Цель заработать " + mission + " гривень");
+// console.log("Период " + period +  " месяцев");
+// console.log("Цель заработать " + mission + " гривень");
 
 
-console.log(addExpenses.toLowerCase().split(', ')); 
+// console.log(addExpenses.toLowerCase().split(', ')); 
 
-let budegetDay = money / 30;
+// let budegetDay = money / 30;
 
-// вопрос о месячном доходе
-money = +prompt('Ваш месячный доход?');
-console.log(money);
+// // вопрос о месячном доходе
+// money = +prompt('Ваш месячный доход?');
+// console.log(money);
 
-// вопрос о статьях расходов
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-console.log(addExpenses.split(', '));
+// // вопрос о статьях расходов
+// addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+// console.log(addExpenses.split(', '));
 
-// вопрос депозите
-deposit = confirm('Есть ли у вас депозит в банке?');
-console.log(deposit);
+// // вопрос депозите
+// deposit = confirm('Есть ли у вас депозит в банке?');
+// console.log(deposit);
 
-// типы данных
-console.log(typeof(money), typeof(income), typeof(deposit));
+// // типы данных
+// console.log(typeof(money), typeof(income), typeof(deposit));
 
-// ещё 4 вопроса о статьях расходов
-let firstMustExpenses = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-console.log(firstMustExpenses);
+// // ещё 4 вопроса о статьях расходов
+// let firstMustExpenses = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+// console.log(firstMustExpenses);
 
-let firstExpensesPrice = +prompt('Во сколько это обойдется?');
-console.log(firstExpensesPrice);
+// let firstExpensesPrice = +prompt('Во сколько это обойдется?');
+// console.log(firstExpensesPrice);
 
-let secondMustExpenses = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-console.log(secondMustExpenses);
+// let secondMustExpenses = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+// console.log(secondMustExpenses);
 
-let secondExpensesPrice = +prompt('Во сколько это обойдется?');
-console.log(secondExpensesPrice);
+// let secondExpensesPrice = +prompt('Во сколько это обойдется?');
+// console.log(secondExpensesPrice);
 
-// рассчет сбережений за месяц
-let budgetMonth = money - (firstExpensesPrice + secondExpensesPrice);
-console.log(budgetMonth);
+// // рассчет сбережений за месяц
+// let budgetMonth = money - (firstExpensesPrice + secondExpensesPrice);
+// console.log(budgetMonth);
 
-// вывод сколько месяцев нужно откладывать
-console.log('цель будет достигнута через ' + Math.ceil(mission / budgetMonth) + ' месяцев.');
+// // вывод сколько месяцев нужно откладывать
+// console.log('цель будет достигнута через ' + Math.ceil(mission / budgetMonth) + ' месяцев.');
 
-// сбережения в день
-budgetDay = budgetMonth / 30;
-console.log(Math.floor(budgetDay));
+// // сбережения в день
+// budgetDay = budgetMonth / 30;
+// console.log(Math.floor(budgetDay));
 
-if(budgetDay >= 800){
-    console.log('Высокий уровень дохода')
-}else if(budgetDay >= 300 && budgetDay < 800){
-    console.log('Средний уровень дохода');
-}else if(budgetDay >= 0 && budgetDay < 300){
-    console.log('Низкий уровень дохода');
-}else if(budgetDay < 0){
-    console.log('Что-то пошло не так')
+// if(budgetDay >= 800){
+//     console.log('Высокий уровень дохода')
+// }else if(budgetDay >= 300 && budgetDay < 800){
+//     console.log('Средний уровень дохода');
+// }else if(budgetDay >= 0 && budgetDay < 300){
+//     console.log('Низкий уровень дохода');
+// }else if(budgetDay < 0){
+//     console.log('Что-то пошло не так')
+// }
+
+// let myFun = function(a, b, plus, minus){
+//     let res = (a > b) ? minus(a, b) : plus(a, b);
+//     console.log(res);
+// }
+
+// myFun(
+// 4,
+// 5,
+// function(a, b){
+//     return a + b;
+// },
+// function(a, b){
+//     return a - b;
+// }
+
+let logUser = function(userData){
+    if(userData === 'admin'){
+        return 'Администратор';
+    }else{
+        return 'Пользователь';
+    }
+};
+
+let inputUser = function(user, callback){
+    let status = 'user';
+    if(user === 'Андрей' || user === 'Василий'){
+        status = 'admin';
+    }
+    return callback(status);
 }
 
+console.log(inputUser('Андрюша', logUser));

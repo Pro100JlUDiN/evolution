5f85cfb8564844fada5f0d2798566a14685ccda3
Fazetdinov Dmitriy <pro100jludin@gmail.com>
2019-07-07 00:25:27 +0500

    пятый урок без доп задачи

--- script/script.js
+++ script/script.js
@@ -1,59 +1,54 @@
-'use stric';
+"use stric";
 // приветствие
-alert('Поздоровался, и шо?');
+alert("Поздоровался, и шо?");
 
-let money; 
-let income = "проточка черенков для лопат";
-let addExpenses = "Корм для меня, Ипотека, Корм для Кошки"; 
-let deposit = false; 
-let mission = 15000000; 
-let period = 6;
+let money, 
+    income = "проточка черенков для лопат",
+    addExpenses = "Корм для меня, Ипотека, Корм для Кошки", 
+    deposit = false, 
+    mission = 15000000, 
+    period = 6;
 
 // вопрос о месячном доходе
 let start = function(){
-    money = prompt('Ваш месячный доход?');
-    console.log(money);
-    while(isNaN(money) || money == "" || money == null){
-        money = prompt('Ваш месячный доход?');
-        console.log(money);
+    do{
+        money = prompt("Ваш месячный доход?");
     }
+    while(isNaN(money) || money == "" || money == null)
+    return +money;
 }
 
 start();
 
 // вопрос о статьях расходов
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую", "кошка, собака, интернет");
 
 // вопрос депозите
-deposit = confirm('Есть ли у вас депозит в банке?');
-
-// ещё 4 вопроса о статьях расходов за месяц
-// let firstMustExpenses = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-
-// let firstExpensesPrice = +prompt('Во сколько это обойдется?');
-
-// let secondMustExpenses = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-
-// let secondExpensesPrice = +prompt('Во сколько это обойдется?');
+deposit = confirm("Есть ли у вас депозит в банке?");
 
 //сумма расходов за месяц
-let firstMustExpenses;
-let secondMustExpenses;
+let firstMustExpenses,
+    secondMustExpenses;
 let getExpensesMonth = function(){
     let sum = 0;
     for(let i = 0; i < 2; i++){
-        if( i === 0){
-            firstMustExpenses = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+        if(i === 0){
+            firstMustExpenses = prompt("Какие обязательные ежемесячные расходы у вас есть?", "кошка, собака, жена");
 
         }else if(i === 1){
-            secondMustExpenses = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+            secondMustExpenses = prompt("Какие обязательные ежемесячные расходы у вас есть?", "магазин, интернет");
         }
         
-        sum += +prompt('Во сколько это обойдется?');
+        
+        sum += +prompt("Во сколько это обойдется?");
+        while(isNaN(sum) || sum == "" || sum == null){
+            sum += +prompt("Во сколько это обойдется?");
+        }
     }
     return sum;
 };
 let costsPerMonth = getExpensesMonth();
+
 
 
 // рассчет сбережений за месяц
@@ -65,13 +60,25 @@ console.log(accumulatedMonth);
 
 // вывод сколько месяцев нужно откладывать
 let getTargetMonth = function(target, save){
-    return "Цель будет достигнута через " + Math.ceil(target / save) + " месяцев.";
+    let charge = Math.ceil(target / save);
+    if(charge < 0){
+        return "Цель не будет достигнута.";
+    }
+    return "Цель будет достигнута через " + charge + " месяцев.";
+    
 };
 console.log(getTargetMonth(mission, accumulatedMonth))
 
 // сбережения в день
-budgetDay = accumulatedMonth / 30;
-console.log(Math.floor(budgetDay));
+let getBudgetDay = function(save, month){
+    let savePerDay = Math.floor(save / month);
+    if(savePerDay < 0){
+        return "Что-то пошло не так!";
+    }
+    return savePerDay;
+}
+let budgetDay = getBudgetDay(accumulatedMonth, 30);
+console.log(budgetDay);
 
 let showTypeOf = function(a, b, c){
     let arr = [];

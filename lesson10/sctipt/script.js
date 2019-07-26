8356c679638cc973b2d18dc1411531bd37ac40d2
Fazetdinov Dmitriy <pro100jludin@gmail.com>
2019-07-26 20:01:57 +0500

    10 урок без доп задачи

--- lesson10/sctipt/script.js
+++ lesson10/sctipt/script.js
@@ -1,1 +1,44 @@
 'use-stric';
+
+
+let blockDiv = document.createElement("div");
+let ourScript = document.querySelector("script");
+
+
+let blockParagrapg = document.createElement("p");
+
+
+function DomElement(selector, heigth, width, bg, fontSize){
+
+    // this.selector = selector;
+    if(selector[0] === "."){
+        blockDiv.className = selector.substring(1); 
+        document.body.insertBefore(blockDiv, ourScript);
+
+        blockDiv.style.cssText = 
+        "height:" + heigth + "px;" +
+        "width:" + width + "px;" +
+        "background-color: "+ bg+";" +
+        "font-size:" + fontSize + "px;"
+
+        blockDiv.textContent = "Йоу КУ!";
+    }else if(selector[0] === "#"){
+        blockParagrapg.id = selector.substring(1);
+        document.body.insertBefore(blockParagrapg, ourScript);
+
+        blockParagrapg.style.cssText = 
+        "height:" + heigth + "px;" +
+        "width:" + width + "px;" +
+        "background-color: "+ bg+";" +
+        "font-size:" + fontSize + "px;"
+        blockParagrapg.textContent = "Да Нет";
+    }
+    
+ 
+}
+
+
+
+let house = new DomElement("#NoDa", 100, 100, "green", 32);
+
+

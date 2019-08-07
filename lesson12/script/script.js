8703c0cf4e0c39153b16e34d1b6bae1060800650
Fazetdinov Dmitriy <pro100jludin@gmail.com>
2019-08-07 22:07:19 +0500

    11 урок, первая часть без доп задачи

--- lesson12/script/script.js
+++ lesson12/script/script.js
@@ -1,5 +1,62 @@
-// let count = 0;
-// let idInterval = setInterval(function(){
-//     count ++;
-//     console.log("DaDa " + count);
-// },3000);
+window.addEventListener("DOMContentLoaded", function(){
+    "use strict";
+    
+    // таймер
+    function countTimer(deadline){
+        let timerHours = document.querySelector("#timer-hours"),
+            timerMinutes = document.querySelector("#timer-minutes"),
+            timerSeconds = document.querySelector("#timer-seconds");
+
+        function getTimeRemaining(){
+            let dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds =  Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours =  Math.floor(timeRemaining / 60 / 60);
+
+
+                return{seconds, minutes,  hours, timeRemaining};
+        }
+        
+        
+        function updateClock(){
+            let timer = getTimeRemaining();
+            let idInterval = 0;
+            
+            // console.log(timerHours.textContent.length);
+            // console.log(indexOfElements);
+            
+            
+            timerHours.textContent = timer.hours;
+            timerMinutes.textContent = timer.minutes;
+            timerSeconds.textContent = timer.seconds;
+            
+            if(timerHours.textContent.length < 2){
+                timerHours.textContent = `0${timer.hours}`;
+            }
+            if(timerMinutes.textContent.length < 2){                
+                timerMinutes.textContent = `0${timer.minutes}`;
+            }
+            if(timerSeconds.textContent.length < 2){
+                timerSeconds.textContent = `0${timer.seconds}`;
+            }
+
+            // console.log(timerHours.textContent);
+            
+            if(timer.timeRemaining > 0){
+                idInterval = setInterval(updateClock, 1000);
+            }else if(timer.timeRemaining < 0){
+                clearInterval(idInterval);
+
+                timerHours.textContent = "00";
+                timerMinutes.textContent = "00";
+                timerSeconds.textContent = "00";
+            }
+        }            
+            
+        updateClock();
+    }
+    countTimer("8 august 2019 ");
+
+});

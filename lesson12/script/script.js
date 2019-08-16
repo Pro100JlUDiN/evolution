19f3dcc92a301a2cb25963810d4f5a0d8dff6436
Fazetdinov Dmitriy <pro100jludin@gmail.com>
2019-08-16 12:43:13 +0500

    13 урок без доп задачи

--- lesson12/script/script.js
+++ lesson12/script/script.js
@@ -2,7 +2,8 @@ window.addEventListener("DOMContentLoaded", function(){
     "use strict";
     
     // таймер
-    function countTimer(deadline){
+    const countTimer = (deadline)=>{
+        
         let timerHours = document.querySelector("#timer-hours"),
             timerMinutes = document.querySelector("#timer-minutes"),
             timerSeconds = document.querySelector("#timer-seconds");
@@ -15,18 +16,13 @@ window.addEventListener("DOMContentLoaded", function(){
                 minutes = Math.floor((timeRemaining / 60) % 60),
                 hours =  Math.floor(timeRemaining / 60 / 60);
 
-
-                return{seconds, minutes,  hours, timeRemaining};
+            return{seconds, minutes,  hours, timeRemaining};
         }
         
         
         function updateClock(){
             let timer = getTimeRemaining();
             let idInterval = 0;
-            
-            // console.log(timerHours.textContent.length);
-            // console.log(indexOfElements);
-            
             
             timerHours.textContent = timer.hours;
             timerMinutes.textContent = timer.minutes;
@@ -41,9 +37,6 @@ window.addEventListener("DOMContentLoaded", function(){
             if(timerSeconds.textContent.length < 2){
                 timerSeconds.textContent = `0${timer.seconds}`;
             }
-
-            // console.log(timerHours.textContent);
-            
             if(timer.timeRemaining > 0){
                 idInterval = setInterval(updateClock, 1000);
             }else if(timer.timeRemaining < 0){
@@ -56,7 +49,61 @@ window.addEventListener("DOMContentLoaded", function(){
         }            
             
         updateClock();
-    }
+    };
     countTimer("8 august 2019 ");
 
+    // меню
+    const tuggleMenu = ()=>{
+        const btnMenu = document.querySelector(".menu"),
+              menu = document.querySelector("menu"),
+              btnClose = document.querySelector(".close-btn"),
+              menuItems = document.querySelectorAll("ul > li"),
+              screenHeight = screen.height,  //812
+              screenWidth = screen.width;    //375
+
+        //добавление классов с анимацией или без
+        const handlerMenu = ()=>{
+            if(screenWidth < 376 && screenHeight < 813){
+                if(!menu.style.transform || menu.style.transform === `translateX(-100%)`){
+                    menu.style.transform = `translateX(0%)`;
+                }else{
+                    menu.style.transform = `translateX(-100%)`;
+                }
+                
+            }else{
+                menu.classList.toggle("active-menu");
+            }
+        };
+
+        btnMenu.addEventListener("click", ()=>{
+            handlerMenu();
+        });
+
+        btnClose.addEventListener("click",()=>{
+            handlerMenu();
+        });
+        
+        menuItems.forEach((elem)=>{
+            elem.addEventListener("click",()=>handlerMenu());
+        });
+
+    };
+    tuggleMenu();
+
+    // блок с полями реквизитов
+    const tugglePopUp = ()=>{
+        const popup = document.querySelector(".popup"),
+              popUpBtn = document.querySelectorAll(".popup-btn"),
+              popUpClose = document.querySelector(".popup-close");
+
+        popUpBtn.forEach((elem)=>{
+            elem.addEventListener("click", ()=>{
+                popup.style.display = "block";
+            });
+        });
+        popUpClose.addEventListener("click",()=>{
+            popup.style.display = "none";
+        });
+    };
+    tugglePopUp();
 });

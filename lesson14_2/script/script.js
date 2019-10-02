392c379c07a064c0c5c15d0fe364d431bace9bd7
Fazetdinov Dmitriy <pro100jludin@gmail.com>
2019-10-02 15:34:26 +0500

    15.5 урок, калькулятор

--- lesson14_2/script/script.js
+++ lesson14_2/script/script.js
@@ -320,17 +320,56 @@ window.addEventListener("DOMContentLoaded", (()=>{
     };       
     changePhoto();
 
-    //настройка калькулятора
-    const couterOptions = ()=>{
-        const calcBlock = document.querySelector(".calc-block");
+    //калькулятор
+    const calc = (price = 100)=>{
+        const calcBlock = document.querySelector(".calc-block"),
+              calcType = document.querySelector(".calc-type"),
+              calcSquare = document.querySelector(".calc-square"),
+              calcDay = document.querySelector(".calc-day"),
+              calcCount = document.querySelector(".calc-count"),
+              totalValue = document.getElementById("total");
         
-        calcBlock.addEventListener('input', (event)=>{
-            let target = event.target;
+        const countSum = ()=>{
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                  squareValue = calcSquare.value;
+            
+            if(calcCount.value > 1){
+                countValue += (calcCount.value - 1) / 10;
+            }
 
+            if(calcDay.value && calcDay.value < 5){
+                dayValue *= 2;
+            }else if(calcDay.value && calcDay.value < 10){
+                dayValue *= 1.5;
+            }
+
+            if(typeValue && squareValue){
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+        };
+
+        //запрет ввода букв
+        calcBlock.addEventListener('input', ()=>{
+            let target = event.target;
             if(target.closest("[type = number]")){
-                target.value = target.value.replace(/[^\S]/gi, "");
+                target.value = target.value.replace(/\D/g, "");
             }
         });
+        
+        calcBlock.addEventListener("change", ()=>{
+            let target = event.target;
+            
+            if(target.matches("select") || target.matches("input")){
+                countSum();
+            }
+
+        });
+        
     };
-    couterOptions();
+    calc(100);
 }));
